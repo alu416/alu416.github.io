de56019d25f5468cd05b43ebfb5720cbf0f061fc
Andy Lu <alu416@gmail.com>
2017-08-09 23:52:42 +0800

    made it non zoomable

--- js/grayscale.js
+++ js/grayscale.js
@@ -50,7 +50,7 @@ function init() {
         center: new google.maps.LatLng(22.654389, 120.291744), // New York
 
         // Disables the default Google Maps UI components
-        disableDefaultUI: true,
+        disableDefaultUI: false,
         scrollwheel: false,
         draggable: false,
 
